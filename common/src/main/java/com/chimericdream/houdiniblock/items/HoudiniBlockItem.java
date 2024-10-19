9fbda5a4c1fe29457f6724504dadd2d1626bd2de
Bill Parrott <bill@chimericdream.com>
2024-10-18 23:32:42 -0500

    feat: re-add cl, no AW this time

--- common/src/main/java/com/chimericdream/houdiniblock/items/HoudiniBlockItem.java
+++ common/src/main/java/com/chimericdream/houdiniblock/items/HoudiniBlockItem.java
@@ -1,5 +1,6 @@
 package com.chimericdream.houdiniblock.items;
 
+import com.chimericdream.lib.text.TextHelpers;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.component.DataComponentTypes;
@@ -17,7 +18,6 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
@@ -51,7 +51,7 @@ public class HoudiniBlockItem extends BlockItem {
         NbtCompound nbt = stack.getOrDefault(DataComponentTypes.CUSTOM_DATA, DEFAULT_NBT).copyNbt();
         PlacementMode currentMode = PlacementMode.valueOf(nbt.getString("houdini_placement_mode"));
 
-        tooltip.add(Text.translatable(TOOLTIP_KEYS.get(currentMode)).formatted(Formatting.AQUA).formatted(Formatting.ITALIC));
+        tooltip.add(TextHelpers.getTooltip(TOOLTIP_KEYS.get(currentMode)));
     }
 
     public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
